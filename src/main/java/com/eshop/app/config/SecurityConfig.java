dd199f63005fe58dc8d7cb6d409bf37eb22773ab
GrEFeRFeeD <azofer77@gmail.com>
2022-12-12 18:11:50 +0200

    :sparkles: Enable CORS for all endpoints.

--- src/main/java/com/eshop/app/config/SecurityConfig.java
+++ src/main/java/com/eshop/app/config/SecurityConfig.java
@@ -1,6 +1,5 @@
 package com.eshop.app.config;
 
-import com.eshop.app.security.CorsEshopFilter;
 import com.eshop.app.security.FilterChainExceptionHandler;
 import com.eshop.app.security.JwtAccessDeniedHandler;
 import com.eshop.app.security.JwtAuthenticationEntryPoint;
@@ -20,7 +19,6 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.security.web.header.writers.StaticHeadersWriter;
-import org.springframework.security.web.session.SessionManagementFilter;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 /**
